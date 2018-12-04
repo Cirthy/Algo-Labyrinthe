5438c26e9ab5ab4271910188d7739388e7915e0d
Baptiste <baptiste.beltzer@etu.unilim.fr>
2018-12-04 23:38:58 +0100

    update Makefile & labyrinthe.h

--- labyrinthe.h
+++ labyrinthe.h
@@ -1,63 +1,40 @@
-#ifndef				LABYRINTHE_H
-#define				LABYRINTHE_H
+#ifndef			LABYRINTHE_H
+#define			LABYRINTHE_H
 
-#include			<stdio.h>
-#include			<stdlib.h>
-#include			<time.h>		//pour srand(tinme(NULL))
-#include			<unistd.h>		//pour usleep()
-#include 			<string.h>		//pour strcmp
+#include		<stdio.h>
+#include		<stdlib.h>
+#include		<time.h>		//pour srand(tinme(NULL))
+#include		<unistd.h>		//pour usleep()
+#include 		<string.h>		//pour strcmp
 
-#define				WALL_PROB		3 //probabilité de mettre un mur lors de la géné := 1 / WALL_PROB
+#define			WALL_PROB		3 //probabilité de mettre un mur lors de la géné := 1 / WALL_PROB
 
 //outil de mise en forme coloré
-/*#define				set_color(param) 	printf("\033[%sm",param)
-#define 			BLOOD 			"1"  // gras
-#define 			ITALIC			"3"  // italique
-#define				UNDER			"4"  // souligné
-#define 			WINKER 			"5"  // clignotant
-#define 			DEFAULT 		"0"  // par défaut
-#define 			GREEN 			"32" // vert
-#define 			BLUE 			"34" // bleu
-#define 			CYAN 		 	"36" // cyan
-#define 			RED 			"31" // rouge
-#define 			MAGENTA			"35" // magenta
-#define 			YELLOW 			"14" // jaune
-#define 			BROWN 			"33" // marron
-*/
 
-#define 			set_color(att, fg) printf("\033[%d;%dm", att, fg + 30)
+#define 		set_color(att, fg) 	printf("\033[%d;%dm", att, fg + 30)
 
-#define RESET		0
-#define DEFAULT		0
-#define BLOOD 		1
-#define 			ITALIC			3  // italique
-#define UNDERLINE 	4
-#define 			WINKER 			5  // clignotant
-#define REVERSE		7
+#define 		RESET			0
+#define 		DEFAULT			0
+#define 		BLOOD 			1
+#define			ITALIC			3  // italique
+#define 		UNDERLINE 		4
+#define 		WINKER 			5  // clignotant
+#define 		REVERSE			7
 
 
-#define BLACK 		0
-#define RED			1
-#define GREEN		2
-#define YELLOW		3
-#define BLUE		4
-#define MAGENTA		5
-#define CYAN		6
-#define LIGHT_GREY 	7
-#define	WHITE		8
-
-
-
-
-
-
-char* dragon[15];
-char* knight[10];
-char* lab[6];
-char* oratoire[6];
+#define 		BLACK 			0
+#define 		RED				1
+#define 		GREEN			2
+#define 		YELLOW			3
+#define 		BLUE			4
+#define 		MAGENTA			5
+#define 		CYAN			6
+#define 		LIGHT_GREY 		7
+#define			WHITE			8
 
 
 enum { LW = 1, BW = 2, RW = 4, TW = 8 };
+
 
 typedef struct		position
 {
@@ -77,12 +54,22 @@ typedef	struct		labyrinthe
 
 typedef struct  	path
 {
-	/*struct path 	*precedent;
-	position		actuel;
-	struct path		*suivant;*/
 	int				length;
 	position		*cells;
 }					path;
+
+char* dragon[15];
+position pos_dragon;
+char* knight[10];
+position pos_knight;
+char* lab[6];
+position pos_lab;
+char* oratoire[6];
+position pos_oratoire;
+
+
+
+
 
 
 
@@ -95,7 +82,7 @@ char	right_wall(unsigned short cell);
 char	top_wall(unsigned short cell);
 void	temp_display(labyrinthe L);
 void	display(labyrinthe L);
-void	displayV2(labyrinthe L);
+void	displayV2(labyrinthe L, char mode, path* plusCourt);
 void	creating_display(labyrinthe L, int half);
 void	create_lab(labyrinthe *L, int lab_height, int lab_width);
 void	init_displayer();
@@ -112,14 +99,14 @@ int		distance(labyrinthe L, position p);
 int		init_distances(labyrinthe L);
 int		actualize_distance(labyrinthe L, position s1, position s2);
 unsigned int cell(labyrinthe L, position p);
-int mark (labyrinthe L, position p);
-int is_marked (labyrinthe L, position p);
-int in_tab(position p, position* V, int size);
-int pos_equal(position s1, position s2);
+int 	mark (labyrinthe L, position p);
+int 	is_marked (labyrinthe L, position p);
+int 	in_tab(position p, position* V, int size);
+int 	pos_equal(position s1, position s2);
 position copy_pos(position s);
-int display_pos(position p);
-int display_tab(position* T, int size_T);
-path BFS(labyrinthe L);
+int 	display_pos(position p);
+int 	display_tab(position* T, int size_T);
+path 	BFS(labyrinthe L);
 
 
 void	display_marked(labyrinthe L);
