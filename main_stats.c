1b173c52e16ebd5aec57830960e28b6f7300dcc2
Baptiste <baptiste.beltzer@etu.unilim.fr>
2018-12-18 08:28:33 +0100

    small upadate in main_stats.c

--- main_stats.c
+++ main_stats.c
@@ -1,9 +1,10 @@
 #include	"labyrinth.h"
 
 #define 	NOMBRE_LAB				1000
-#define		TAILLE_LAB_CARRE		4000
-#define		TYPE 					'r'		// 'i'tératif
+#define		TAILLE_LAB_CARRE		1000
+#define		TYPE 					'g'		// 'i'tératif
 											// 'r'écursif
+											// 'g' juste générétion
 
 
 int	main()
@@ -17,30 +18,40 @@ int	main()
 
 	for(int i=0 ; i<NOMBRE_LAB ; i++)
 	{
-		printf("test %3d ->",i);
+		printf("test %3d -> ",i);
 		fflush(stdout);
 
 		create_alea_lab(&L, TAILLE_LAB_CARRE, TAILLE_LAB_CARRE);
 
-		printf(" généré ->");
+		printf("généré -> ");
 		fflush(stdout);
 
 		if (TYPE == 'r')
+		{
+			// récursif
 			p = pathfinding(&L, LARGEUR);
-		else
+			printf("longeur %4d -> ", p.length);
+			fflush(stdout);
+			if (p.length != -1 && p.length != DISTANCE_MAX)
+				free(p.cells);
+		}
+		if (TYPE == 'i')
+		{
+			// itératif
 			p = BFS(L);
+			printf("longeur %4d -> ", p.length);
+			fflush(stdout);
+			if (p.length != -1 && p.length != DISTANCE_MAX)
+				free(p.cells);
+		}
 
-		printf(" longeur %4d ->", p.length);
-		fflush(stdout);
 
-		if (p.length != -1)
-			free(p.cells);
 		for(int j = 0 ; j < L.height ; j++)
 			free(L.grid[j]);
 		free(L.grid);
 
-		printf(" free\n");
+		printf("free\r                                          \r");
+		fflush(stdout);
 	}
-
 	return 0;
 }
